272bb4f14a9a0b67d455305e006f9f0704d581a3
guillermo <guillermobaldan@gmail.com>
2024-06-06 11:37:56 +0200

    side menu added when hamburguer icon is clicked

--- JS/index.v2.js
+++ JS/index.v2.js
@@ -21,4 +21,62 @@ dropdownMenu.addEventListener('mouseout', () => {
     // Ocultamos el menú dropdown al sacar el mouse sobre el div .container-dropdown-menu
     dropdownMenu.style.display = 'none';
 });
+
+/* Dropdown menu */
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdownMenu = document.querySelector('.container-dropdown-menu');
+    const mobileNavbar = document.querySelector('.mobile-navbar'); // Navbar para resoluciones móviles
+
+    if (window.innerWidth <= 600) {
+        // Mostrar menú al pasar el ratón por encima de la barra de navegación
+        mobileNavbar.addEventListener('mouseover', () => {
+            dropdownMenu.style.display = 'flex';
+        });
+
+        // Ocultar menú al sacar el ratón de la barra de navegación
+        mobileNavbar.addEventListener('mouseout', () => {
+            dropdownMenu.style.display = 'none';
+        });
+
+        // Mostrar menú al pasar el ratón por encima del menú
+        dropdownMenu.addEventListener('mouseover', () => {
+            dropdownMenu.style.display = 'flex';
+        });
+
+        // Ocultar menú al sacar el ratón del menú
+        dropdownMenu.addEventListener('mouseout', () => {
+            dropdownMenu.style.display = 'none';
+        });
+    }
+});/* END Dropdown menu */
+
+
 /* END Menú barra de navegación */
+
+/* Menú lateral */
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.querySelector('.menu-icon');
+    const sideMenu = document.querySelector('.side-menu');
+
+    menuIcon.addEventListener('click', () => {
+        sideMenu.classList.toggle('show');
+    });
+});/* END Menú lateral */
+
+
+
+// Script para mostrar el ancho de la pantalla en la consola
+document.addEventListener('DOMContentLoaded', () => {
+    // Función para mostrar el ancho de la pantalla
+    function mostrarAnchoPantalla() {
+        console.log(`El ancho de la pantalla es: ${window.innerWidth}px`);
+    }
+
+    // Llamar a la función al cargar la página
+    mostrarAnchoPantalla();
+
+    // También puedes llamar a la función cuando se redimensiona la ventana
+    window.addEventListener('resize', mostrarAnchoPantalla);
+});
+
+
